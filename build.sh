6a1306fb1d2f9be2b8312599dd7deea3741f98f1
cubee <cubeecb+github@gmail.com>
2026-01-28 10:02:51 +1100

    make testpak, add doublejump alt to 4-3

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 export OGMO_BUILD_SCRIPT=1
 
 # for linux users; edit these to point to your install's levels folder
-./pakify.py "/mnt/big-chungus/projects/monogame/Ninja_Cat_Remewstered/Ninja Cat Desktop 383/Content/levels/"
-./pakify.py "/mnt/big-chungus/projects/monogame/Ninja_Cat_Remewstered/Ninja Cat Desktop 383/bin/Debug/net8.0/Content/levels/"
+GAMEPATH="/mnt/big-chungus/projects/monogame/Ninja_Cat_Remewstered/Ninja Cat Desktop 383"
+./pakify.py "$GAMEPATH/Content/levels/" basepak outset sequel finale
+./pakify.py "$GAMEPATH/bin/Debug/net8.0/Content/levels/" basepak outset sequel finale testpak
